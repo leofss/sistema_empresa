d370b8bb84a37d81368f3040ffcce74defd74bcc
leonardo fernandes <leofernandess11@gmail.com>
2022-02-17 15:11:54 -0300

    Implementação das classes e regras de negócio

---
+++ Entities/OutsrcEmployee.cs
@@ -0,0 +1,24 @@
+﻿namespace sistema_empresa.Entities
+{
+    class OutsrcEmployee : Employee
+    {
+        public double AddCharge { get; set; }
+
+
+        public OutsrcEmployee()
+        {
+
+        }
+
+        public OutsrcEmployee(double addCharge, string name, int hours, double valuePerHour)
+            : base(name, hours, valuePerHour)
+        {
+            AddCharge = addCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AddCharge; //110% * addcharge
+        }
+    }
+}
