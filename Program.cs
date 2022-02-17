cc8c7cc5eed336b0d010633867d693cd9f40ff7a
leonardo fernandes <leofernandess11@gmail.com>
2022-02-17 14:56:35 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sistema_empresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
