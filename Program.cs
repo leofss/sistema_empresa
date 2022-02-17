188d2e6c4cda7a14a796269e4a145bf32d966cea
leonardo fernandes <leofernandess11@gmail.com>
2022-02-17 15:34:05 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
+using sistema_empresa.Entities;
+using System.Globalization;
 
 namespace sistema_empresa
 {
@@ -7,7 +9,43 @@ namespace sistema_empresa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Employee> list = new List<Employee>(); //isntanceia e inicia a lista
+
+            Console.Write("Insira numero de funcionarios: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"insira informações sobre o funcionario {i}");
+                Console.WriteLine("terceirizado? (s/n)");
+                char sn = char.Parse(Console.ReadLine());
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Hora: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.WriteLine("Value por hora: ");
+                double valueperhour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(sn == 's')
+                {
+                    Console.WriteLine("Despesa adicional: ");
+                    double addcharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsrcEmployee(addcharge, name, hours, valueperhour));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valueperhour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Folha de pagamento: ");
+            foreach(Employee emp in list)
+            {
+                Console.WriteLine($"{emp.Name} - ${emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+
+
+
         }
     }
 }
